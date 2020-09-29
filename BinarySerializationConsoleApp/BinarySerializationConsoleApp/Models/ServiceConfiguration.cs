cf6038aa37242a8645dd9389078ab4f5b20d498c
devandteach <devandteach-ccj@yahoo.com>
2020-09-29 18:27:12 -0400

    stripped ISerializable support

--- BinarySerializationConsoleApp/BinarySerializationConsoleApp/Models/ServiceConfiguration.cs
+++ BinarySerializationConsoleApp/BinarySerializationConsoleApp/Models/ServiceConfiguration.cs
@@ -8,33 +8,21 @@ using System.Threading.Tasks;
 namespace BinarySerializationConsoleApp.Models
 {   
     [Serializable]
-    public class ServiceConfiguration //: ISerializable
+    public class ServiceConfiguration 
     {
 
         [NonSerialized]
         private Guid _internalId;
-        public string ConfigName { get; set; }
+
         public string DatabaseHostName { get; set; }
         public string ApplicationDataPath { get; set; }
+        public string ConfigName { get; set; }
+      
 
         public ServiceConfiguration()
         {
         }
-        public ServiceConfiguration(SerializationInfo info, StreamingContext ctxt)
-        {
-            this.ConfigName
-               = info.GetValue("ConfigName", typeof(string)).ToString();
-            this.DatabaseHostName
-               = info.GetValue("DatabaseHostName", typeof(string)).ToString();
-            this.ApplicationDataPath
-               = info.GetValue("ApplicationDataPath", typeof(string)).ToString();
-        }
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("ConfigName", this.ConfigName);
-            info.AddValue("DatabaseHostName", this.DatabaseHostName);
-            info.AddValue("ApplicationDataPath", this.ApplicationDataPath);
-        }
+       
 
 
 
