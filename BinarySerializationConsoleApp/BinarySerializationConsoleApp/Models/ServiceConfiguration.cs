ecd506d78faefde115cf845cca31773e87ba363c
devandteach <devandteach-ccj@yahoo.com>
2020-09-29 18:17:18 -0400

    Initial Commit

---
+++ BinarySerializationConsoleApp/BinarySerializationConsoleApp/Models/ServiceConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerializationConsoleApp.Models
+{   
+    [Serializable]
+    public class ServiceConfiguration //: ISerializable
+    {
+
+        [NonSerialized]
+        private Guid _internalId;
+        public string ConfigName { get; set; }
+        public string DatabaseHostName { get; set; }
+        public string ApplicationDataPath { get; set; }
+
+        public ServiceConfiguration()
+        {
+        }
+        public ServiceConfiguration(SerializationInfo info, StreamingContext ctxt)
+        {
+            this.ConfigName
+               = info.GetValue("ConfigName", typeof(string)).ToString();
+            this.DatabaseHostName
+               = info.GetValue("DatabaseHostName", typeof(string)).ToString();
+            this.ApplicationDataPath
+               = info.GetValue("ApplicationDataPath", typeof(string)).ToString();
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("ConfigName", this.ConfigName);
+            info.AddValue("DatabaseHostName", this.DatabaseHostName);
+            info.AddValue("ApplicationDataPath", this.ApplicationDataPath);
+        }
+
+
+
+        public static ServiceConfiguration Default
+        {
+            get
+            {
+                return new ServiceConfiguration()
+                {
+                    ConfigName = "Development",
+                    DatabaseHostName = "dev.db.server",
+                    ApplicationDataPath = "C://path/to/app/data"
+                };
+            }
+        }
+
+    }
+
+}
+
---
+++ BinarySerializationConsoleApp/BinarySerializationConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using BinarySerializationConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySerializationConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the object you want to serialize.
+            ServiceConfiguration config = ServiceConfiguration.Default;
+            // Create the formatter you want to use to serialize the object.
+            IFormatter formatter = new BinaryFormatter();
+            // Create the stream that the serialized data will be buffered to.
+            FileStream buffer = File.Create($@"{System.Environment.CurrentDirectory}\config.txt");
+            // Invoke the Serialize method.
+            formatter.Serialize(buffer, config);
+            // Close the stream.
+            buffer.Close();
+        }
+    }
+}
